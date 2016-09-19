4c72ba3dc6ac2b62a3e6e532a1d9c0df794b9e7e
Lukaka4331 <105021044@live.asia.edu.tw>
2016-09-19 16:05:30 +0800

    ex finish

--- week01_20160919/src/ex/ex03_105021044.java
+++ week01_20160919/src/ex/ex03_105021044.java
@@ -2,6 +2,7 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入數字 a 與 b，程式 計算 a 的 b 次方後輸出結果 (請使用 Math.pow(a,b) 工具)
  * Date: 2016/09/19
+ * Author: 105021044 鐘岑哲
  */
 import java.util.Scanner;
 public class ex03_105021044 {
